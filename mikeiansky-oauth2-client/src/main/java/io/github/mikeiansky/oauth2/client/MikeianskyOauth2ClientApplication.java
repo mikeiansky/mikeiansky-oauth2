477d3cf3004d7d3d5d8727c7605f601ffe9ebae8
mikeian <zhouwenxiang@ciwei.net>
2025-03-20 20:10:20 +0800

    add oauth2 authorization server and oauth2 client;

---
+++ mikeiansky-oauth2-client/src/main/java/io/github/mikeiansky/oauth2/client/MikeianskyOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package io.github.mikeiansky.oauth2.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikeianskyOauth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikeianskyOauth2ClientApplication.class, args);
+    }
+
+}
---
+++ mikeiansky-oauth2-client/src/test/java/io/github/mikeiansky/oauth2/client/MikeianskyOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mikeiansky.oauth2.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeianskyOauth2ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
