6cf414f494a6775254984ceffd5783d186af249e
mikeian <zhouwenxiang@ciwei.net>
2025-04-16 12:17:35 +0800

    add spring security sample module;

---
+++ sample/mikeiansky-spring-security-sample/src/main/java/io/github/mikeiansky/sample/spring/security/MikeianskySpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.mikeiansky.sample.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikeianskySpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikeianskySpringSecuritySampleApplication.class, args);
+    }
+
+}
---
+++ sample/mikeiansky-spring-security-sample/src/test/java/io/github/mikeiansky/sample/spring/security/MikeianskySpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mikeiansky.sample.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeianskySpringSecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
