d86a791e1a6b6ff78dcb2335355f2bd20228d4a1
mikeian <zhouwenxiang@ciwei.net>
2025-06-27 18:44:05 +0800

    login 配置

--- mikeiansky-oauth2-authorization-server/src/main/java/io/github/mikeiansky/oauth2/authorization/server/config/AppConfig.java
+++ mikeiansky-oauth2-authorization-server/src/main/java/io/github/mikeiansky/oauth2/authorization/server/config/AppConfig.java
@@ -1,14 +1,18 @@
 package io.github.mikeiansky.oauth2.authorization.server.config;
 
+import io.github.mikeiansky.oauth2.authorization.server.filter.LoginFilter;
 import io.github.mikeiansky.oauth2.authorization.server.service.RedisSecurityContextRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.savedrequest.CookieRequestCache;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * @author mike ian
@@ -23,22 +27,28 @@ public class AppConfig {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
                 OAuth2AuthorizationServerConfigurer.authorizationServer();
         httpSecurity
-                .with(authorizationServerConfigurer, authorizationServer->
-                        authorizationServer.oidc(Customizer.withDefaults())
-                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/oauth2/**"),   // 授权端点
+                        new AntPathRequestMatcher("/login"),       // 登录页面
+                        new AntPathRequestMatcher("/logout")       // 登出端点
+                ))
+                .with(authorizationServerConfigurer, authorizationServer -> {
+                    authorizationServer.oidc(Customizer.withDefaults());
+                })
                 .requestCache(requestCache -> {
                     requestCache.requestCache(new CookieRequestCache());
                 })
-                .securityContext(securityContext-> {
+                .securityContext(securityContext -> {
                     securityContext.securityContextRepository(new RedisSecurityContextRepository(redisTemplate));
                 })
                 .authorizeHttpRequests(auth -> {
                     auth.anyRequest().authenticated();
                 })
                 .formLogin(Customizer.withDefaults())
-                .rememberMe(remember->{
+                .rememberMe(remember -> {
                     remember.key("remember-me");
                 })
+//                .addFilterBefore(new LoginFilter(), UsernamePasswordAuthenticationFilter.class)
         ;
         return httpSecurity.build();
     }
